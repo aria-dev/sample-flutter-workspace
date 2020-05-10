b259d95deb0d01dfffcd94c0a64778cb9185bde3
aria-dev <iamshanuuknow@outlook.com>
2020-05-10 03:00:59 +0000

    New Repo

---
+++ file.sh
@@ -0,0 +1,6 @@
+echo "# sample-flutter-workspace" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git remote add origin https://github.com/aria-dev/sample-flutter-workspace.git
+git push -u origin master
